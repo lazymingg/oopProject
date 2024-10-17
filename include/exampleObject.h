70ae983efcb4fe770ae61cda0fd66264e1b4c906
lazymingg <nvminh232@clc.fitus.edu.vn>
2024-10-17 21:10:40 +0700

    adding point class

--- include/exampleObject.h
+++ include/exampleObject.h
@@ -12,4 +12,6 @@ class Animal
         void speak();
 };
 
+
+
 #endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "rapidxml.hpp"
+#include "include/Point.h"
 #include <fstream>
 using namespace rapidxml;
 using namespace std;
